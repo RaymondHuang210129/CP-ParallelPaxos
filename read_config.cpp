39e3f86107b6f3fb5142b9977f790ababd6c7a18
RaymondHuang210129 <huan1763@purdue.edu>
2022-04-26 22:10:12 -0400

    fix bug at read_config.cpp

--- read_config.cpp
+++ read_config.cpp
@@ -57,7 +57,7 @@ void read_config(std::vector<Entry> &replicas, std::vector<Entry> &leaders,
 
     // lines in section 3
     while (std::getline(config, line)) {
-        
+        if (line == "") continue;
         line = line.substr(0, line.find("//"));
         if (line.substr(0, 4) == "----") {
             break;
@@ -72,7 +72,7 @@ void read_config(std::vector<Entry> &replicas, std::vector<Entry> &leaders,
         std::string hostAddress = host.substr(0, host.find(":"));
         int hostPort = stoi(host.substr(host.find(":")+1));
         int threadStartPort = stoi(startPort);
-        leaders.push_back(Entry(hostAddress, hostPort, threadStartPort, numThreads));
+        acceptors.push_back(Entry(hostAddress, hostPort, threadStartPort, numThreads));
     }
 }
 
@@ -84,4 +84,23 @@ Entry getMyEntry(std::vector<Entry> entries, std::string address, int port){
     }
     std::cout << "unable to find my entry" << std::endl;
     assert(false);
+}
+
+int read_test() {
+    std::vector<Entry> replicas;
+    std::vector<Entry> leaders;
+    std::vector<Entry> acceptors;
+    read_config(replicas, leaders, acceptors);
+    for (int i = 0; i < replicas.size(); i++) {
+        std::cout << replicas[i].address << " " << replicas[i].hostPort << " " << replicas[i].threadStartPort << " " << replicas[i].numThreads << std::endl;
+    }
+    std::cout << "--" << std::endl; 
+    for (int i = 0; i < leaders.size(); i++) {
+        std::cout << leaders[i].address << " " << leaders[i].hostPort << " " << leaders[i].threadStartPort << " " << leaders[i].numThreads << std::endl;
+    }
+    std::cout << "--" << std::endl; 
+    for (int i = 0; i < acceptors.size(); i++) {
+        std::cout << acceptors[i].address << " " << acceptors[i].hostPort << " " << acceptors[i].threadStartPort << " " << acceptors[i].numThreads << std::endl;
+    }
+    return 0;
 }
